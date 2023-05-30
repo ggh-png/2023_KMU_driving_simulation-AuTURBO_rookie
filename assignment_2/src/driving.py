c3f56bdf760cc72b4dea4e689febbbba9d33dec1
ggh-png <0380089@gmail.com>
2023-05-30 03:59:36 +0000

    pid control node

--- assignment_2/src/driving.py
+++ assignment_2/src/driving.py
@@ -5,13 +5,11 @@
 # 함께 사용되는 각종 파이썬 패키지들의 import 선언부
 #=============================================
 import numpy as np
+import cv2
 import rospy, time
-import numpy as np
-
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
 from xycar_msgs.msg import xycar_motor
-from std_msgs.msg import Float64
-from geometry_msgs.msg import Twist
-
 from math import *
 import signal
 import sys
@@ -28,71 +26,106 @@ def signal_handler(sig, frame):
 
 signal.signal(signal.SIGINT, signal_handler)
 
+#=============================================
+# 프로그램에서 사용할 변수, 저장공간 선언부
+#=============================================
+image = np.empty(shape=[0]) # 카메라 이미지를 담을 변수
+bridge = CvBridge()
+motor = None # 모터 토픽을 담을 변수
 
-# =============================================
-# 차선을 따라가는 알고리즘을 구현한 클래스
-class ControlLane():
-    def __init__(self):
-        self.motor_msg = xycar_motor()
-        # 중앙 차선을 받아옴
-        self.sub_lane = rospy.Subscriber('/xycar/center_lane', Float64, self.cbFollowLane, queue_size = 1)
-        self.sub_max_vel = rospy.Subscriber('xycar_motor', xycar_motor, self.cbGetMaxVel, queue_size = 1)
-        self.pub_cmd_vel = rospy.Publisher('xycar_motor', xycar_motor, queue_size = 1)
-        
-        # 초기 오차값을 0으로 설정
-        self.lastError = 0
-        # 최대 속도를 0.1로 설정
-        self.MAX_VEL = 50
+#=============================================
+# 프로그램에서 사용할 상수 선언부
+#=============================================
+CAM_FPS = 30    # 카메라 FPS - 초당 30장의 사진을 보냄
+WIDTH, HEIGHT = 640, 480    # 카메라 이미지 가로x세로 크기
 
-        # ROS 종료시 속도를 0으로 설정
-        rospy.on_shutdown(self.fnShutDown)
-    # 최대 속도를 받아옴
-    def cbGetMaxVel(self, max_vel_msg):
-        self.MAX_VEL = max_vel_msg.speed
+#=============================================
+# 콜백함수 - 카메라 토픽을 처리하는 콜백함수
+# 카메라 이미지 토픽이 도착하면 자동으로 호출되는 함수
+# 토픽에서 이미지 정보를 꺼내 image 변수에 옮겨 담음.
+#=============================================
+def img_callback(data):
+    global image
+    image = bridge.imgmsg_to_cv2(data, "bgr8")
 
-    # lane center를 받아옴 & PID 제어
-    def cbFollowLane(self, desired_center):
-        #  현제 차선의 중앙값을 받아옴
-        center = desired_center.data
-        #  오차값을 계산
-        error = center
-        #  PID 제어
-        Kp = 0.025
-        # Ki = 0.0001 + Ki * (error + self.lastError)
-        Kd = 0.00
-        # 제어값을 계산
-        angular_z = Kp * error + Kd * (error - self.lastError) 
-        # 오차값을 저장
-        self.lastError = error
-        
-        # 모터 메세지를 생성하고 발행
-        motor_msg = xycar_motor()
-        # 최대 속도를 넘지 않도록 설정
-        motor_msg.angle = int(angular_z)
-        # int(-max(angular_z, -50) if angular_z < 0 else -min(angular_z, 50))
-        # motor_msg.speed = int(min(self.MAX_VEL * ((1 - abs(error) / 500) ** 2.2), 0.05))
-        motor_msg.speed = 10
-        self.pub_cmd_vel.publish(motor_msg)
+#=============================================
+# 모터 토픽을 발행하는 함수
+# 입력으로 받은 angle과 speed 값을
+# 모터 토픽에 옮겨 담은 후에 토픽을 발행함.
+#=============================================
+def drive(angle, speed):
 
-    # ROS 종료시 속도를 0으로 설정
-    def fnShutDown(self):
-        rospy.loginfo("Shutting down. cmd_vel will be 0")
-        motor_msg = xycar_motor()
-        motor_msg.angle = 0
-        motor_msg.speed = 0
-        self.pub_cmd_vel.publish(motor_msg)
-    # 메인 함수
-    def main(self):
-        rospy.spin()
-    
-        
-        
+    global motor
 
+    motor_msg = xycar_motor()
+    motor_msg.angle = angle
+    motor_msg.speed = speed
+
+    motor.publish(motor_msg)
+
+#=============================================
+# 실질적인 메인 함수
+# 카메라 토픽을 받아 각종 영상처리와 알고리즘을 통해
+# 차선의 위치를 파악한 후에 조향각을 결정하고,
+# 최종적으로 모터 토픽을 발행하는 일을 수행함.
+#=============================================
+def start():
+
+    # 위에서 선언한 변수를 start() 안에서 사용하고자 함
+    global motor, image
+
+    #=========================================
+    # ROS 노드를 생성하고 초기화 함.
+    # 카메라 토픽을 구독하고 모터 토픽을 발행할 것임을 선언
+    #=========================================
+    rospy.init_node('driving')
+    motor = rospy.Publisher('xycar_motor', xycar_motor, queue_size=1)
+    rospy.Subscriber("/usb_cam/image_raw/",Image, img_callback)
+
+    print ("----- Xycar self driving -----")
+
+    # 첫번째 카메라 토픽이 도착할 때까지 기다림.
+    rospy.wait_for_message("/usb_cam/image_raw/", Image)
+
+    #=========================================
+    # 메인 루프
+    # 카메라 토픽이 도착하는 주기에 맞춰 한번씩 루프를 돌면서
+    # "이미지처리 + 차선위치찾기 + 조향각 결정 + 모터토픽 발행"
+    # 작업을 반복적으로 수행함.
+    #=========================================
+    while not rospy.is_shutdown():
+
+        # 이미지 처리를 위해 카메라 원본 이미지를 img에 복사 저장한다.
+        img = image.copy()
+
+        # img를 화면에 출력한다.
+        #cv2.imshow("CAM View", img)
+        #cv2.waitKey(1)
+
+        #=========================================
+        # 핸들 조향각 값인 angle값 정하기.
+        # 차선의 위치 정보를 이용해서 angle값을 설정함.
+        #=========================================
+
+        # 핸들을 얼마나 꺾을지 결정
+        angle = 0
+
+        #=========================================
+        # 차량의 속도 값인 speed값 정하기.
+        # 주행 속도를 조절하기 위해 speed값을 설정함.
+        #=========================================
+
+        # 주행 속도를 결정
+        speed = 10
+
+        # drive() 호출. drive()함수 안에서 모터 토픽이 발행됨.
+        drive(angle, speed)
+
+
+#=============================================
 # 메인 함수
+# 가장 먼저 호출되는 함수로 여기서 start() 함수를 호출함
+# start() 함수가 실질적인 메인 함수임.
 #=============================================
 if __name__ == '__main__':
-    rospy.init_node('control_lane')
-
-    node = ControlLane()
-    node.main()
-
+    start()
---
+++ assignment_2/src/lane_control.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#=============================================
+# 함께 사용되는 각종 파이썬 패키지들의 import 선언부
+#=============================================
+import numpy as np
+import rospy, time
+import numpy as np
+
+from xycar_msgs.msg import xycar_motor
+from std_msgs.msg import Float64
+from geometry_msgs.msg import Twist
+
+from math import *
+import signal
+import sys
+import os
+
+#=============================================
+# 터미널에서 Ctrl-c 키입력이로 프로그램 실행을 끝낼 때
+# 그 처리시간을 줄이기 위한 함수
+#=============================================
+def signal_handler(sig, frame):
+    time.sleep(3)
+    os.system('killall -9 python rosout')
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+
+# =============================================
+# 차선을 따라가는 알고리즘을 구현한 클래스
+class ControlLane():
+    def __init__(self):
+        self.motor_msg = xycar_motor()
+        # 중앙 차선을 받아옴
+        self.sub_lane = rospy.Subscriber('/xycar/center_lane', Float64, self.cbFollowLane, queue_size = 1)
+        self.sub_max_vel = rospy.Subscriber('xycar_motor', xycar_motor, self.cbGetMaxVel, queue_size = 1)
+        self.pub_cmd_vel = rospy.Publisher('xycar_motor', xycar_motor, queue_size = 1)
+        
+        # 초기 오차값을 0으로 설정
+        self.lastError = 0
+        # 최대 속도를 0.1로 설정
+        self.MAX_VEL = 50
+
+        # ROS 종료시 속도를 0으로 설정
+        rospy.on_shutdown(self.fnShutDown)
+    # 최대 속도를 받아옴
+    def cbGetMaxVel(self, max_vel_msg):
+        self.MAX_VEL = max_vel_msg.speed
+
+    # lane center를 받아옴 & PID 제어
+    def cbFollowLane(self, desired_center):
+        #  현제 차선의 중앙값을 받아옴
+        center = desired_center.data
+        #  오차값을 계산
+        error = center
+        #  PID 제어
+        Kp = 0.025
+        # Ki = 0.0001 + Ki * (error + self.lastError)
+        Kd = 0.00
+        # 제어값을 계산
+        angular_z = Kp * error + Kd * (error - self.lastError) 
+        # 오차값을 저장
+        self.lastError = error
+        
+        # 모터 메세지를 생성하고 발행
+        motor_msg = xycar_motor()
+        # 최대 속도를 넘지 않도록 설정
+        motor_msg.angle = int(-max(angular_z, -50) if angular_z < 0 else -min(angular_z, 50))
+        # motor_msg.speed = int(min(self.MAX_VEL * ((1 - abs(error) / 500) ** 2.2), 0.05))
+        motor_msg.speed = 10
+        self.pub_cmd_vel.publish(motor_msg)
+
+    # ROS 종료시 속도를 0으로 설정
+    def fnShutDown(self):
+        rospy.loginfo("Shutting down. cmd_vel will be 0")
+        motor_msg = xycar_motor()
+        motor_msg.angle = 0
+        motor_msg.speed = 0
+        self.pub_cmd_vel.publish(motor_msg)
+    # 메인 함수
+    def main(self):
+        rospy.spin()
+    
+ 
+
+# 메인 함수
+#=============================================
+if __name__ == '__main__':
+    rospy.init_node('control_lane')
+
+    node = ControlLane()
+    node.main()
+
