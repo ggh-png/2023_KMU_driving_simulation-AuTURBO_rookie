2369d680a10923fbc61448e7ab29fbbc49f9ca91
Seongjun Choi <sjchoi.dp@gmail.com>
2023-06-20 08:31:58 +0900

    Finish: 230619 straight line: 43, curve: 42

--- assignment_2/src/driving.py
+++ assignment_2/src/driving.py
@@ -14,7 +14,6 @@ from math import *
 import signal
 import sys
 import os
-import matplotlib.pyplot as plt
 
 from preprocessor import PreProcessor
 
@@ -36,14 +35,17 @@ signal.signal(signal.SIGINT, signal_handler)
 image = np.empty(shape=[0]) # 카메라 이미지를 담을 변수
 #bridge = CvBridge()
 motor = None # 모터 토픽을 담을 변수
+errorPrev = 0 
+proportional_gain = 0.52 
+integral_gain = 0.00025 #  0.0000002
+derivative_gain = 10.00 # 8.00
+error_sum = 0
 
 #=============================================
 # 프로그램에서 사용할 상수 선언부
 #=============================================
 CAM_FPS = 30    # 카메라 FPS - 초당 30장의 사진을 보냄
 WIDTH, HEIGHT = 640, 480    # 카메라 이미지 가로x세로 크기
-error_prev = 0
-error_sum = 0
 
 #=============================================
 # 콜백함수 - 카메라 토픽을 처리하는 콜백함수
@@ -53,8 +55,7 @@ error_sum = 0
 def img_callback(data):
     global image
     image = np.frombuffer(data.data, dtype=np.uint8).reshape(data.height, data.width, -1)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    #image = bridge.imgmsg_to_cv2(data, "bgr8")
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # R
 
 #=============================================
 # 모터 토픽을 발행하는 함수
@@ -72,27 +73,26 @@ def drive(angle, speed):
     motor.publish(motor_msg)
 
 #=============================================
-# angle을 제어하는 PID 제어기
-# 입력으로 받은 reference_input과 feedback_input 값을 바탕으로 
-# error을 계산하고 이를 PID 제어기에 넣어 조향각을 결정함.
+# PID 제어 함수
+# 목표 값과 현재 값을 받아
+# 최종 PID 제어 출력을 생성한다.
 #=============================================
-def calculate_PID(reference_input, feedback_input):
-    global error_sum, error_prev
-    
-    proportional_gain = 0.52 #0.7
-    integral_gain = 0.0000002 #0.0001
-    derivative_gain = 8.00#8.5
-    
-    error = reference_input - feedback_input   # Compute the error
-    error_sum = error_sum + error            # Compute the error sum
-    proportional_output = proportional_gain * error  # Compute the proportional output
-    integral_output = integral_gain*error_sum   # Compute the integral output
-    derivative_output = derivative_gain * (error - error_prev) # Compute the derivative output
-    error_prev = error
 
-    # Compute the pre-saturated output
-    presaturated_output = int(proportional_output + integral_output + derivative_output)
-    return presaturated_output
+def calculate_PID(reference_input, feedback_input):
+    # 위에서 선언한 변수를 calculate_PID() 안에서 사용하고자 함
+    global error_sum
+    global errorPrev
+    
+    error = reference_input - feedback_input   # 에러 계산
+    error_sum = error_sum + error            # 에러합 계산(적분)
+    proportional_output = proportional_gain * error  # 비례(P) 제어 값
+    integral_output = integral_gain*error_sum   # 적분(I) 제어 값
+    derivative_output = derivative_gain * (error - errorPrev) # 미분(D) 제어 값
+    errorPrev = error 
+
+    # 최종 PID 제어 출력 값 
+    output = int(proportional_output + integral_output + derivative_output)
+    return output
 
 #=============================================
 # 실질적인 메인 함수
@@ -100,11 +100,13 @@ def calculate_PID(reference_input, feedback_input):
 # 차선의 위치를 파악한 후에 조향각을 결정하고,
 # 최종적으로 모터 토픽을 발행하는 일을 수행함.
 #=============================================
-def start():
-    roi_height = 200
-    roi_width = 640
 
-    pre_module = PreProcessor(roi_height, roi_width)
+def start():
+
+    roi_height = 200 # roi 높이
+    roi_width = 640 # roi 폭
+
+    pre_module = PreProcessor(roi_height, roi_width) # 영상처리 객체 생성
 
     # 위에서 선언한 변수를 start() 안에서 사용하고자 함
     global motor, image
@@ -128,45 +130,50 @@ def start():
     # "이미지처리 + 차선위치찾기 + 조향각 결정 + 모터토픽 발행"
     # 작업을 반복적으로 수행함.
     #========================================
+
     while not rospy.is_shutdown():
+       
         # 이미지 처리를 위해 카메라 원본 이미지를 img에 복사 저장한다.
         img = image.copy()
 
         rows, cols = img.shape[:2] # 이미지의 height, width 정보를 받아온다.
         
-        cv2.imshow("origin", img)
+        cv2.imshow("origin", img) # 원본 영상 디스플레이
         cv2.waitKey(1)
 
-        warped_img = pre_module.warp_perspect(img)
-        # cv2.imshow("warped_img", warped_img)
+        warped_img = pre_module.warp_perspect(img) # Inverse Perspective Mapping(IPM)을 이용해 Bird Eye View 변환
+        #cv2.imshow("warped_img", warped_img) # Bird Eye View 영상 디스플레이
 
+        white_parts = pre_module.color_filter(warped_img) # HSV 변환후 흰색 픽셀만 필터링
+        #cv2.imshow("white_parts", white_parts) # 흰색 픽셀 필터링 영상 디스플레이
 
-        white_parts = pre_module.color_filter(warped_img)
-        # cv2.imshow("white_parts", white_parts)
+        msk, lx, ly, rx, ry = pre_module.sliding_window(white_parts) # Sliding window 알고리즘으로 차선 검출
+        
+        #overlay_img = pre_module.overlay_line(warped_img, lx, ly, rx, ry) # 2차 다항식으로 차선 피팅후 오버레이 
 
-        msk, lx, ly, rx, ry = pre_module.sliding_window(white_parts)
+        #=========================================
+        # 제어기가 추종할 값인 target값 정하기.
+        # 차선 검출의 결과에 따라 추종할 목표 값을 설정함.
+        #=========================================
 
-        if len(lx) > 2 and len(rx) > 2 :
-            target = (lx[len(lx) // 2] + rx[len(rx) // 2]) // 2
-            speed = 40
+        if len(lx) > 2 and len(rx) > 2 : # 왼쪽 차선과 오른쪽 차선 둘다 있을떄는 직선 구간으로 판단
+            target = (lx[len(lx) // 2] + rx[len(rx) // 2]) // 2 # 왼쪽차선과 오른쪽 차선의 중앙값을 추종하는 목표 값으로 설정
+            speed = 43 # 직선 구간은 속도를 43으로 설정
         elif len(lx) > 2 and len(rx) == 0: # 왼쪽 차선만 있을때
-            if len(lx) > 2 and len(ly) > 2: # 오른쪽 차선 각도 구하기
-                w = lx[-1] - lx[0]
+            if len(lx) > 2 and len(ly) > 2: # 왼쪽 차선 각도 구하기
+                w = lx[-1] - lx[0] 
                 h = ly[-1] - ly[0]
                 rad = atan2(h, w)
                 line_angle = abs(degrees(rad))
 
                 print(f"left angle:{line_angle}")
 
-                if line_angle > 120: # 왼쪽으로 기울어져 있으면
-                    target = 275
+                if line_angle > 90 + 25: # 왼쪽으로 일정 각도 이상 기울어져 있으면 급격한 코너로 판단
+                    target = 250 # 왼쪽으로 회전
                     print("left!!!")
-                else:
-                    target = lx[len(lx) // 2] + 50
-            else:
-                target = lx[len(lx) // 2] + 50
-            #print("left only")
-            speed = 35
+                else: # 코너가 아닌 왼쪽 차선만 있으면 
+                    target = lx[len(lx) // 2] + 50 # 왼쪽 차선과 일정 간격 유지
+            speed = 42 # 속도를 42으로 설정
         elif len(lx) == 0 and len(rx) > 2: # 오른쪽 차선만 있을때
             if len(rx) > 2 and len(ry) > 2: # 오른쪽 차선 각도 구하기
                 w = rx[-1] - rx[0]
@@ -176,27 +183,27 @@ def start():
 
                 print(f"right angle:{line_angle}")
 
-                if line_angle < 57: # 오른쪽으로 기울어져 있으면
-                    target = 375  
+                if line_angle < 90 - 25: # 오른쪽으로 일정 각도 이상 기울어져 있으면 급격한 코너로 판단 57
+                    target = 400 # 오른쪽으로 회전  
                     print("right!!!")
-                else:
-                    target = rx[len(rx) // 2] - 50
-            else:
-                target = rx[len(rx) // 2] - 50
-            speed = 35
-            #print("right only")
+                else: # 코너가 아닌 오른쪽 차선만 있으면 
+                    target = rx[len(rx) // 2] - 50 # 오른쪽 차선과 일정 간격 유지
+            speed = 42 # 속도를 42으로 설정
+
         print(f"target: {target}")
 
         if target != None:
-            cv2.circle(msk, (target, 120), 3, (255, 0, 0), 4)
-        cv2.circle(msk, (320, 120), 3, (0, 0, 255), 2)
-        cv2.imshow("Lane Detection - Sliding Windows", msk)
-
+            cv2.circle(msk, (target, 120), 3, (255, 0, 0), 4) # 추종 목표 값 점으로 표시 
+        cv2.circle(msk, (320, 120), 3, (0, 0, 255), 2) # 차량 중앙 점으로 표시
+        cv2.imshow("Lane Detection - Sliding Windows", msk) # Sliding window 디스플레이
+        
         #=========================================
         # 핸들 조향각 값인 angle값 정하기.
-        # 차선의 위치 정보를 이용해서 angle값을 설정함.
+        # 차선의 위치 정보와 PID 제어기를 이용해서 angle값을 설정함.
         #=========================================
-        angle = calculate_PID(target, 320)
+
+        angle = calculate_PID(target, 320) # 핸들을 얼마나 꺾을지 결정
+        #print(f"angle: {angle}")
 
         # drive() 호출. drive()함수 안에서 모터 토픽이 발행됨.
         drive(angle, speed)
--- assignment_2/src/preprocessor.py
+++ assignment_2/src/preprocessor.py
@@ -1,41 +1,65 @@
+#=============================================
+# 함께 사용되는 각종 파이썬 패키지들의 import 선언부
+#=============================================
+
 import numpy as np
 import cv2
 import os
 import matplotlib.pyplot as plt
 
-# 이미지 전처리와 차선 검출에 관련된 메서드들을 포함하는 클래스
+#=============================================
+# 영상처리를 위한 클래스
+# Bird Eye View 변환
+# HSV 필터링
+# 차선 시작점 검출 
+# Sliding window 알고리즘(차선 검출) 등 멤버 함수 존재 
+#=============================================
+
+
 class PreProcessor():
-    # 해당 클래스의 인스턴스를 초기화
     def __init__(self, roi_height, roi_width):
-        self.roi_height = roi_height
-        self.roi_width = roi_width
-        self.left_line_detect_flag = False
+        self.roi_height = roi_height # ROI 높이 설정
+        self.roi_width = roi_width # ROI 폭 설정
+        self.left_line_detect_flag = False 
         self.right_line_detect_flag = False
 
-    # 입력 이미지를 원근 변환(perspective transformation)을 통해 변형시키는 메서드
-    # src와 dst는 변환에 사용되는 좌표들을 나타내는 배열
+    #=============================================
+    # Bird Eye View 변환 함수
+    # src에서 dst로 변환하는 변환행렬을 구하고,
+    # 이를 이용해 Bird Eye View를 생성한다.
+    #=============================================
+
     def warp_perspect(self, img):
-        # src와 dst는 변환에 사용되는 좌표들을 나타내는 배열
-        src = np.float32([[0, 0], [0, self.roi_height-50 ],  [self.roi_width, 0], [640, self.roi_height-50 ]])
-        dst = np.float32([[0, 0], [272, self.roi_height-50 ], [self.roi_width, 0], [367, self.roi_height-50 ]])
-        
-        # 변환 행렬 M과 역변환 행렬 Minv를 생성
-        M = cv2.getPerspectiveTransform(src, dst) # The transformation matrix
-        Minv = cv2.getPerspectiveTransform(dst, src) # Inverse transformation
+        src = np.float32([[0, 0], [0, self.roi_height-50 ],  [self.roi_width, 0], [640, self.roi_height-50 ]]) # Perspective transform을 위한 src 좌표 설정
+        dst = np.float32([[0, 0], [272, self.roi_height-50 ], [self.roi_width, 0], [367, self.roi_height-50 ]]) # Perspective transform을 위한 dst 좌표 설정
+        M = cv2.getPerspectiveTransform(src, dst) # 변환 행렬 생성 (src -> dst)
+        #Minv = cv2.getPerspectiveTransform(dst, src) # Inverse transformation
 
-        cv2.circle(img, (int(src[0][0]), int(src[0][1])), 1, (255,0 ,0), 10)
-        cv2.circle(img, (int(src[1][0]), int(src[1][1])), 1, (0,255 ,0), 10)
-        cv2.circle(img, (int(src[2][0]), int(src[2][1])), 1, (0,0 ,255), 10)
-        cv2.circle(img, (int(src[3][0]), int(src[3][1])), 1, (255,255 ,0), 10)
+        # src, dst 시각화(디버깅 용)
 
-        img = img[280:(280+self.roi_height-50), 0:self.roi_width] # Apply np slicing for ROI crop
+        # cv2.circle(img, (int(src[0][0]), int(src[0][1])), 1, (255,0 ,0), 10)
+        # cv2.circle(img, (int(src[1][0]), int(src[1][1])), 1, (0,255 ,0), 10)
+        # cv2.circle(img, (int(src[2][0]), int(src[2][1])), 1, (0,0 ,255), 10)
+        # cv2.circle(img, (int(src[3][0]), int(src[3][1])), 1, (255,255 ,0), 10)
 
-        # 이미지를 원근 변환
-        warped_img = cv2.warpPerspective(img, M, (self.roi_width, self.roi_height-50), flags=cv2.INTER_LINEAR) # Image warping
+
+        # cv2.circle(img, (int(dst[0][0]), int(dst[0][1])), 1, (255,0 ,0), 10)
+        # cv2.circle(img, (int(dst[1][0]), int(dst[1][1])), 1, (0,255 ,0), 10)
+        # cv2.circle(img, (int(dst[2][0]), int(dst[2][1])), 1, (0,0 ,255), 10)
+        # cv2.circle(img, (int(dst[3][0]), int(dst[3][1])), 1, (255,255 ,0), 10)
+
+        img = img[280:(280+self.roi_height-50), 0:self.roi_width] # ROI 적용
+
+        warped_img = cv2.warpPerspective(img, M, (self.roi_width, self.roi_height-50), flags=cv2.INTER_LINEAR) # 이미지 워핑으로 Bird Eye View 생성
+
         return warped_img
 
-    # 입력 이미지에서 흰색 부분을 추출하는 메서드
-    # 입력 이미지를 BGR에서 HSV 색 공간으로 변환한 후, 미리 설정된 범위(흰색)에 해당하는 색상만 마스킹하여 추출
+    #=============================================
+    # HSV 필터링 함수
+    # 색공간을 BGR에서 HSV로 변환하고 흰색 픽셀의 마스크를 구한다.
+    # 마스크를 이용해 흰색 픽셀만 추출하고 Gray 이미지로 변환함.  
+    #=============================================
+
     def color_filter(self, img):
         # BGR to HSV 변환
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
@@ -51,130 +75,175 @@ class PreProcessor():
         # 이미지에서 흰색 부분만 추출
         white_parts = cv2.bitwise_and(img, img, mask=mask)
 
-        # Convert the image to grayscale
+        # BGR이미지를 Gray이미지로 변환
         white_parts = cv2.cvtColor(white_parts, cv2.COLOR_BGR2GRAY)
 
         return white_parts
 
-    # 입력 이미지에서 차선의 히스토그램을 생성하고, 히스토그램에서 왼쪽 차선과 오른쪽 차선의 기준점을 찾는 메서드
-    # np.sum() 함수를 사용하여 이미지의 아래 절반부분에서 각 열의 합을 구한 뒤, 
-    # 히스토그램의 중간 지점을 계산하여 이를 기준으로 왼쪽과 오른쪽 차선의 기준점을 계산하고 반환
+    #=============================================
+    # 차선 시작점 검출 함수
+    # Sliding window 알고리즘의 초기 탐색의 시작점을 구하는 역할.
+    # 차선이 있을것으로 추정되는 이미지 하단부분의 히스토그램을 이용해 초기 탐색점을 구한다.  
+    #=============================================
+
     def hist_line_peak(self, img):
-        histogram = np.sum(img[140:, :], axis=0)
-        midpoint = np.int(histogram.shape[0]/2)
+        histogram = np.sum(img[140:, :], axis=0) # X축 히스토그램 계산
+        midpoint = np.int(histogram.shape[0]/2) # 중앙점 계산
 
-        left_hist_result = np.argmax(histogram[:midpoint])
-        right_hist_result = np.argmax(histogram[midpoint:])
+        left_hist_result = np.argmax(histogram[:midpoint]) # 중앙점을 기준으로 히스토그램을 계산해 왼쪽 라인 시작점 구함
+        right_hist_result = np.argmax(histogram[midpoint:]) # 중앙점을 기준으로 히스토그램을 계산해 오른쪽 라인 시작점 구함
 
-        if right_hist_result == 0:
-            right_base = right_hist_result + midpoint +90
+        if right_hist_result == 0: # 오른쪽 차선이 없으면 차량이 왼쪽으로 치우쳐져 있다고 판단
+            right_base = right_hist_result + midpoint +90 # 오른쪽 차선 탐색점을 더 오른쪽으로 설정
         else:
-            right_base = midpoint + right_hist_result
+            right_base = midpoint + right_hist_result # 오른쪽 차선이 존재하면 히스토그램을 찾은 좌표 그대로 사용(중앙값을 기준으로 우측 이미지에서 구했으므로 중앙값을 더해줌)
 
-        if left_hist_result == 0:
-            left_base = left_hist_result + midpoint -90
+        if left_hist_result == 0: # 왼쪽 차선이 없으면 차량이 오른쪽으로 치우쳐져 있다고 판단
+            left_base = left_hist_result + midpoint -90 # 왼쪽 차선 탐색점을 더 왼쪽 설정
         else:
-            left_base = left_hist_result
-
+            left_base = left_hist_result # 왼쪽 차선이 존재하면 히스토그램을 찾은 좌표 그대로 사용 
+        
+        #print(left_base, right_base)
         return left_base, right_base
 
-    # 입력 이미지에서 슬라이딩 윈도우 방식을 사용하여 차선을 검출하는 메서드
-    # 이전에 검출한 차선의 상태를 고려하고, 윈도우를 이용하여 차선을 검출
-    # 검출된 차선의 좌표들을 저장하고, 결과 이미지와 함께 반환
-    def sliding_window(self, img):
-        prev_detect_flag_left = False
-        prev_detect_flag_right = False
-        line_detect_fail_count_left = 0
-        line_detect_fail_count_right = 0
+    #=============================================
+    # 차선검출에 이용되는 Sliding window 알고리즘
+    # 일정 크기의 윈도우를 위로 슬라이딩하면서 차선을 탐색하고, 
+    # 누적된 감지를 기반으로 차선 위치를 결정함.
+    #=============================================
 
-        left_base, right_base = self.hist_line_peak(img)
+    def sliding_window(self, img):
+        prev_detect_flag_left = False # 직전 차선 검출 성공 플래그 False로 세팅 (왼쪽)
+        prev_detect_flag_right = False # 직전 차선 검출 성공 플래그 False로 세팅 (오른쪽)
+        line_detect_fail_count_left = 0 # 차선 검출 실패 카운트 0으로 초기화 (왼쪽)
+        line_detect_fail_count_right = 0 # 차선 검출 실패 카운트 0으로 초기화 (오른쪽)
+
+        left_base, right_base = self.hist_line_peak(img) # hist_line_peak 함수로 슬라이딩 윈도우의 초기 탐색점 결정
 
         #Sliding Window
-        y = 140
-        lx = []
-        ly = []
-        rx = []
-        ry = []
-        window_width = 12
-        window_height = 4
+        y = 140 # 탐색 시작 Y좌표 결정
+        lx = [] # 왼쪽 차선 X좌표 저장 리스트
+        ly = [] # 왼쪽 차선 Y좌표 저장 리스트
+        rx = [] # 오른쪽 차선 X좌표 저장 리스트
+        ry = [] # 오른쪽 차선 Y좌표 저장 리스트
+        window_width = 12 # window 폭
+        window_height = 4 # window 높이
         left_window_n = 0
         right_window_n = 0
-        msk = img.copy()
-        msk = cv2.cvtColor(msk, cv2.COLOR_GRAY2BGR)
+        msk = img.copy() # 차선검출 결과를 디스플레이 하기 위한 이미지 복사
+        msk = cv2.cvtColor(msk, cv2.COLOR_GRAY2BGR) # 컬러 표시를 위해 색공간을 Gray에서 BGR로 변환
 
-        while y>0:
-            
-            if left_window_n < 6:
-                ## Left threshold
-                window = img[y-window_height:y, left_base-window_width:left_base+window_width]
+        while y>0: # window가 이미지 상단에 도달할때까지 반복
+            if left_window_n < 6: # 왼쪽 차선 검출을 위한 window를 5개까지만 허용 
+                window = img[y-window_height:y, left_base-window_width:left_base+window_width] # left window 생성
+                contours, _ = cv2.findContours(window, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) # window 영역에서 contour 검출
 
-                contours, _ = cv2.findContours(window, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-                # print(len(contours))
                 if(len(contours) == 0): # 차선을 못 찾았을떄
                     if prev_detect_flag_left == False: # 차선을 연속으로 못 찾았을떄
-                        line_detect_fail_count_left+=1
+                        line_detect_fail_count_left+=1 # 차선 검출 실패 카운트 증가
                         cv2.circle(msk, (left_base-window_width-20, y-(window_height//2)), 1, (0,50,150), 1)
 
-                    prev_detect_flag_left = False
+                    prev_detect_flag_left = False # 직전 차선 검출 성공 플래그 False로 세팅
                     cv2.circle(msk, (left_base-window_width, y-(window_height//2)), 1, (0,125,125), 1)
                 
-                if line_detect_fail_count_left < 5:
+                if line_detect_fail_count_left < 5: # 차선 검출 실패 카운트가 5 이하일떄 차선으로 판단
                     
                     cv2.circle(msk, (left_base-window_width, y-(window_height//2)), 1, (0,255,0), 1)
 
-                    for contour in contours:
-                        M = cv2.moments(contour)
+                    for contour in contours: 
+                        M = cv2.moments(contour) # contour 모멘트 계산
                         if M["m00"] != 0:
-                            cx = int(M["m10"]/M["m00"])
-                            cy = int(M["m01"]/M["m00"])
-                            lx.append(left_base-window_width + cx)
-                            ly.append(y-(window_height//2))
+                            cx = int(M["m10"]/M["m00"]) # contour 모멘트 X 좌표 계산
+                            cy = int(M["m01"]/M["m00"]) # contour 모멘트 Y 좌표 계산
+                            lx.append(left_base-window_width + cx) # 왼쪽 차선 X좌표 저장 
+                            ly.append(y-(window_height//2)) # 왼쪽 차선 Y좌표 저장 
                             cv2.circle(msk, (left_base-window_width + cx, y-(window_height//2)), 1, (255,0,0), 1)
-                            left_base = left_base-window_width + cx
+                            left_base = left_base-window_width + cx # 다음 차선(위쪽 존재) 검출 윈도우 좌표를 현재 검출된 contour 모멘트를 고려해서 수정 
                             cv2.rectangle(msk, (left_base-window_width,y), (left_base+window_width,y-window_height), (255,0,0), 1)
-                            self.left_line_detect_flag = True
-                            left_window_n += 1         
+                            prev_detect_flag_left = True  # 직전 차선 검출 성공 플래그 True로 세팅
+                            self.left_line_detect_flag = True # 차선 검출 성공 플래그 True로 세팅
+                            left_window_n += 1 # 왼쪽 윈도우 개수 증가        
 
-                if len(lx) < 1:
+                if len(lx) < 1: # 왼쪽 차선을 못찾았을뗴 
                     #print("Left line No")
-                    self.left_line_detect_flag = False
-            
-            ## Right threshold
-            
-            if right_window_n < 6:
-                window = img[y-window_height:y, right_base-window_width:right_base+window_width]
-                contours, _ = cv2.findContours(window, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+                    self.left_line_detect_flag = False # 왼쪽 차선 검출 성공 플래그 False로 세팅
+
+            if right_window_n < 6: # 오른쪽 차선 검출을 위한 window를 5개까지만 허용 
+                window = img[y-window_height:y, right_base-window_width:right_base+window_width] # right window 생성
+                contours, _ = cv2.findContours(window, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) # window 영역에서 contour 검출
 
                 if(len(contours) == 0): # 차선을 못 찾았을떄
                     if prev_detect_flag_right == False: # 차선을 연속으로 못 찾았을떄
-                        line_detect_fail_count_right+=1
+                        line_detect_fail_count_right+=1 # 차선 검출 실패 카운트 증가
                         cv2.circle(msk, (right_base-window_width-20, y-(window_height//2)), 1, (0,255,0), 1)
 
-                    prev_detect_flag_right = False
+                    prev_detect_flag_right = False # 직전 차선 검출 성공 플래그 False로 세팅
                     cv2.circle(msk, (right_base-window_width-10, y-(window_height//2)), 1, (0,255,255), 1)
                 
-                if line_detect_fail_count_right < 5:
+                if line_detect_fail_count_right < 5: # 차선 검출 실패 카운트가 5 이하일떄 차선으로 판단
                     cv2.circle(msk, (right_base-window_width, y-(window_height//2)), 1, (0,0,255), 1)
 
                     for contour in contours:
-                        M = cv2.moments(contour)
+                        M = cv2.moments(contour) # contour 모멘트 계산
                         if M["m00"] != 0:
-                            cx = int(M["m10"]/M["m00"])
-                            cy = int(M["m01"]/M["m00"])
-                            rx.append(right_base-window_width + cx)
-                            ry.append(y-(window_height//2))
+                            cx = int(M["m10"]/M["m00"]) # contour 모멘트 X 좌표 계산
+                            cy = int(M["m01"]/M["m00"]) # contour 모멘트 Y 좌표 계산
+                            rx.append(right_base-window_width + cx) # 오른쪽 차선 X좌표 저장 
+                            ry.append(y-(window_height//2)) # 오른쪽 차선 Y좌표 저장 
                             cv2.circle(msk, (right_base-window_width + cx, y-(window_height//2)), 1, (0,255,0), 1)
-                            right_base = right_base-window_width + cx
+                            right_base = right_base-window_width + cx  # 다음 차선(위쪽 존재) 검출 윈도우 좌표를 현재 검출된 contour 모멘트를 고려해서 수정 
                             cv2.rectangle(msk, (right_base-window_width,y), (right_base+window_width,y-window_height), (0,255,0), 1)
-                            prev_detect_flag_right = True
-                            self.right_line_detect_flag = True
+                            prev_detect_flag_right = True  # 직전 차선 검출 성공 플래그 True로 세팅
+                            self.right_line_detect_flag = True # 차선 검출 성공 플래그 True로 세팅
+                            right_window_n += 1 # 오른쪽 윈도우 개수 증가        
                 
-                if len(rx) < 1:
+                if len(rx) < 1: # 오른쪽 차선을 못찾았을뗴 
                     #print("Right line No")
-                    self.right_line_detect_flag = False
-                right_window_n += 1
+                    self.right_line_detect_flag = False # 오른쪽 차선 검출 성공 플래그 False로 세팅
 
             # cv2.rectangle(msk, (left_base-window_width,y), (left_base+window_width,y-window_height), (0,0,255), 1)
             # cv2.rectangle(msk, (right_base-window_width,y), (right_base+window_width,y-window_height), (0,0,255), 1)
-            y -= window_height
+            y -= window_height # 윈도우 Y좌표 위로 이동
         return msk, lx, ly, rx, ry
+
+    
+
+    #=============================================
+    # 검출된 차선을 화면에 표시
+    # 3차곡선으로 피팅후 다각형으로 화면에 표시함.
+    #=============================================
+
+    def overlay_line(self, warped_img, lx, ly, rx, ry):
+        #Fit a third order polynomial to each
+
+        if len(lx) != 0 and len(ly) != 0: # 왼쪽 차선이 존재하면 
+            left_fit = np.polyfit(ly, lx, 3) # 3차 곡선 피팅
+
+            # 화면에 오버레이를 위한 X, Y값 생성 
+            ploty = np.linspace(0, warped_img.shape[0] - 1, warped_img.shape[0]) 
+            left_lane_fitx = left_fit[0] * ploty ** 3 + left_fit[1] * ploty ** 2 + left_fit[2] * ploty + left_fit[3]
+            #left_lane_fitx = left_fit[0] * ploty ** 2 + left_fit[1] * ploty + left_fit[2] # 2차 곡선일떄
+
+            # 좌우 차선을 연결하기 위해 포인트 생성
+            left_points = np.array([np.transpose(np.vstack([left_lane_fitx, ploty]))])
+
+            # 찾은 라인을 이미지에 그리기
+            cv2.polylines(warped_img, np.int32([left_points]), isClosed=False, color=(255, 0, 0), thickness=5)
+
+        if len(rx) != 0 and len(ry) != 0: # 오른쪽 차선이 존재하면 
+            right_fit = np.polyfit(ry, rx, 3) # 3차 곡선 피팅
+
+            # 화면에 오버레이를 위한 X, Y값 생성
+            ploty = np.linspace(0, warped_img.shape[0] - 1, warped_img.shape[0])
+            right_lane_fitx = right_fit[0] * ploty ** 3 + right_fit[1] * ploty ** 2 + right_fit[2] * ploty + right_fit[3]
+            #right_lane_fitx = right_fit[0] * ploty ** 2 + right_fit[1] * ploty + right_fit[2] # 2차 곡선일떄
+
+            #return lane_fitx, lane_fit
+
+            # 좌우 차선을 연결하기 위해 포인트 생성
+            right_points = np.array([np.flipud(np.transpose(np.vstack([right_lane_fitx, ploty])))])
+
+            # 찾은 라인을 이미지에 그리기
+            cv2.polylines(warped_img, np.int32([right_points]), isClosed=False, color=(0, 255, 0), thickness=5)
+
+        return warped_img
